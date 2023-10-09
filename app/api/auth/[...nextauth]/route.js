79c0ae9e0ecff956eeafa8a966a232068db96c77
Pannu786 <thepannu786@gmail.com>
2023-10-09 17:17:05 +0530

    idk how but created the login/sign in thingy dne

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
@@ -13,9 +11,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
-  async signIn({ profile }) {},
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      //* check if user is already exists
+      const userExits = await User.findOne({ email: profile.email });
+      //* if not, create a new user
+      if (!userExits) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').tolowerCase(),
+          image: profile.image,
+        });
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
